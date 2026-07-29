c0b3b005a31b62af616437448e29200e8d59cf71
clawforpeng <clawforpeng@gmail.com>
2026-07-29 15:11:40 -0400

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.py
@@ -0,0 +1,26 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if not intervals:
+            return True
+        intervals.sort(key=lambda interval: interval.start)
+
+        start, end = intervals[0].start, intervals[0].end
+
+        for i in range(1, len(intervals)):
+            curStart = intervals[i].start
+            curEnd = intervals[i].end
+
+            if curStart < end:
+                return False
+            start = curStart
+            end = curEnd
+        
+        return True
