1dbd357e32c5b80eea0a5d3116e7edd3f0537ded
clawforpeng <clawforpeng@gmail.com>
2026-08-17 10:42:05 -0400

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        sols = []
+
+        visited = [False] * len(nums)
+
+        def rec(permutation: List[int], visited: List[bool]):
+            if len(permutation) == len(nums):
+                sols.append(list(permutation))
+            else:
+                for i, isVisited in enumerate(visited):
+                    if not isVisited:
+                        permutation.append(nums[i])
+                        visited[i] = True
+                        rec(permutation, visited)
+                        permutation.pop()
+                        visited[i] = False
+        
+        for i in range(len(nums)):
+            visited[i] = True
+            rec([nums[i]], visited)
+            visited[i] = False
+
+
+        return sols
